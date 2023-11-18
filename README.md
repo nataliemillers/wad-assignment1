68d46eb195116a4b9a30b640f2eebbfade43b44c
nataliemillers <naaathxlie@gmail.com>
2023-11-18 20:17:51 +0800

    completed gym portal node module

--- README.md
+++ README.md
@@ -1,9 +1,154 @@
-# Assignment 1
+# Gym Portal Node Module
 
-You will only need one file, ie, your node module, for this assignment.
+This Node module simulates a gym portal for personal trainers and members.
 
-In this readme file, describe how to use your node module. It could be similar to **app.js** from Lab2, where you call some functions in your node module and display the output. Describe how to setup your node module, if any. Describe how to call the functions, what parameters required and so on.
+## Compatability
 
-You can press **Ctrl+Shift+V** in this file in Visual Studio Code to see a live preview of the readme file.
+This Node module has been developed and tested with Node.js version 20.9.0 and 21.0.0.
 
-For some tips in formatting text in readme file, refer to https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+## Setup
+
+1. Install Node.js: [Node.js Download](https://nodejs.org/)
+
+2. Clone the repository:
+    ```bash
+    git clone https://github.com/nataliemillers/wad-assignment1.git
+    ```
+
+3. Navigate to the project directory:
+    ```
+    cd wad-assignment1
+    ```
+
+4. Create an `app.js` file in the root folder of the project to call the functions
+
+5. Require the node module in `app.js` by inserting this line of code at the top of the file:
+    ```
+    const gymPortal = require("./natalie_webapi.js);
+    ```
+
+6. Install dependencies:
+    ```
+    npm install
+    npm install nodemon
+    ```
+
+7. Run nodemon in a new terminal to start the module:
+    ```
+    nodemon app.js
+    ```
+
+<br>
+
+## Usage
+### Functions
+1. Add Trainer <br>
+Required parameters: **name**, **specialty** <br>
+Sample usage:
+    ```
+    // Add three trainers into the system
+    gymPortal.addTrainer("Jack Bulldog", "endurance, and flexability");
+    gymPortal.addTrainer("Janice Whale", "strength training, and flexability");
+    gymPortal.addTrainer("Willy Wonka", "weight loss, strength training, flexability, and Horse kicking");
+    ```
+<br>
+
+2. Display All Trainers <br>
+No parameters required. <br>
+Call the function:
+    ```
+    // View details of all personal trainers
+    const allTrainers = gymPortalController.viewAllTrainers();
+
+    console.log("Registered Personal Trainers (PT):");
+    allTrainers.forEach(trainer => {
+        console.log(`NAME: ${trainer.name}, SPECIALTY: ${trainer.specialty}`);
+    });
+    ```
+    Expected Output:
+    ```
+    Registered Personal Trainers (PT):
+    NAME: Jack Bulldog, SPECIALTY: endurance, and flexability
+    NAME: Janice Whale, SPECIALTY: strength training, and flexability
+    NAME: Willy Wonka, SPECIALTY: weight loss, strength training, flexability, and Horse kicking
+    ```
+<br>
+
+3. Trainer Select Sessions <br>
+Reqyured parameters: **name** (must be existing trainer), **time slot** (can enter more than 3) <br>
+Sample usage:
+    ```
+    // User must select at least 3 time slots
+    gymPortal.trainerSelectSessions('Jack Bulldog', '05:00 - 07:00', '07:00 - 09:00', '09:00 - 11:00');
+    gymPortal.trainerSelectSessions('Janice Whale', '05:00 - 07:00', '11:00 - 13:00','13:00 - 15:00', '15:00 - 17:00');
+    gymPortal.trainerSelectSessions('Willy Wonka', '17:00 - 19:00', '19:00 - 21:00', '21:00 - 23:00');
+    ```
+<br>
+
+4. Display All Available Sessions <br>
+No parameters required. <br>
+Call the function:
+    ```
+    // View all available personal trainer sessions
+    const availableSessions = gymPortal.viewAvailablePTsessions();
+    console.log("Available Personal Trainer Sessions:");
+    availableSessions.forEach(session => {
+        console.log(session);
+    });
+    ```
+    Note: To view updated available sessions, move this function call after the bookGym function (bookGym booking must be successful to have an update). <br>
+
+    Expected output:
+    ```
+    Available Personal Trainer Sessions:
+    Jack Bulldog is available for session 05:00 - 07:00
+    Jack Bulldog is available for session 07:00 - 09:00
+    Jack Bulldog is available for session 09:00 - 11:00
+    Janice Whale is available for session 05:00 - 07:00
+    Janice Whale is available for session 11:00 - 13:00
+    Janice Whale is available for session 13:00 - 15:00
+    Janice Whale is available for session 15:00 - 17:00
+    Willy Wonka is available for session 17:00 - 19:00
+    Willy Wonka is available for session 19:00 - 21:00
+    Willy Wonka is available for session 21:00 - 23:00
+    ```
+<br>
+
+5. Book Gym Slot <br>
+Required parameters: **name**, **contact**(optional), **fitness goal**, **desired time slot** <br>
+Sample usage:
+    ```
+    // User book a session with a PT based on fitness goals and preferred timing OR User book a general session without a PT
+    const bookGym1 = gymPortal.bookGym('Kacey More', '8123456', 'flexability', '09:00 - 11:00');
+    console.log(bookGym1);
+
+    // User book general session without specific fitness goal but with personal trainer
+    const bookGym2 = gymPortal.bookGym('Red Matt', '82593939', '', '21:00 - 23:00');
+    console.log(bookGym2);
+    ```
+
+    Expected output:
+    ```
+    Hi Kacey More, your booking is successful! Jack Bulldog will be your personal trainer for your fitness goal: FLEXABILITY at 09:00 - 11:00.
+    Hi Red Matt, your booking is successful! You have a general session at 21:00 - 23:00 with no targetted fitness goal with Willy Wonka.
+    ```
+<br>
+
+6. Display All Members Scheduled Timings <br>
+No parameters required. <br>
+Call the function:
+    ```
+    // Display all members registered with their registered timing
+    const allMembersData = gymPortal.displayAllMemberSchedule();
+    console.log("Members' Schedule:");
+    allMembersData.forEach(memberData => {
+        console.log(`NAME: ${memberData.name}, TIMING: ${memberData.registeredTiming}`);
+    });
+    ```
+
+    Expected output:
+    ```
+    Members' Schedule:
+    NAME: Kacey More, TIMING: 09:00 - 11:00
+    NAME: Red Matt, TIMING: 21:00 - 23:00
+    ```
--- natalie_webapi.js
+++ natalie_webapi.js
@@ -1,10 +1,213 @@
-module.exports = {
-    // Explain what function A does
-    functionA() {
-        return 1+2;
-    },
-    // Explain what function B does
-    functionB() {
-        console.log("Hello function B");
+// Gym Portal Node Module
+
+// Simulate database using arrays
+const trainerController = {
+    trainers: [],
+    PTsessions: [
+        { time: '05:00 - 07:00', booked: false },
+        { time: '07:00 - 09:00', booked: false },
+        { time: '09:00 - 11:00', booked: false },
+        { time: '11:00 - 13:00', booked: false },
+        { time: '13:00 - 15:00', booked: false },
+        { time: '15:00 - 17:00', booked: false },
+        { time: '17:00 - 19:00', booked: false },
+        { time: '19:00 - 21:00', booked: false },
+        { time: '21:00 - 23:00', booked: false },
+    ],
+    bookedSessions: [],
+};
+
+const memberController = {
+    members: [],
+    timeslots: [
+        { time: '05:00 - 07:00', booked: false },
+        { time: '07:00 - 09:00', booked: false },
+        { time: '09:00 - 11:00', booked: false },
+        { time: '11:00 - 13:00', booked: false },
+        { time: '13:00 - 15:00', booked: false },
+        { time: '15:00 - 17:00', booked: false },
+        { time: '17:00 - 19:00', booked: false },
+        { time: '19:00 - 21:00', booked: false },
+        { time: '21:00 - 23:00', booked: false },
+    ],
+}
+
+// Function 1: Add newly hired trainer into the system
+function addTrainer(name, specialty) {
+    // create a new trainer object with an empty array for selected sessions
+    const newTrainer = {
+        name: name,
+        specialty: specialty,
+        selectedSessions: [],
+    };
+    // add the newly created trainer to the list of trainers in the simulated database
+    trainerController.trainers.push(newTrainer);
+}
+
+// Function 2: Allow trainer to select at least 3 time slots to achieve minimum working hours
+function trainerSelectSessions(name, ...selectedSessions) {
+    // find the trainer in the list of registered trainers
+    const trainer = trainerController.trainers.find(trainer => trainer.name === name);
+
+    // check if trainer is found
+    if (trainer) {
+        // check if the trainer has selected at least 3 time slots
+        if (selectedSessions.length >= 3) {
+            // assign the selected time slots to the trainer
+            trainer.selectedSessions = selectedSessions;
+        } else {
+            // display error message if trainer has not selected enough time slots
+            console.log("You are required to select at least 3 time slots.");
+        }
+    } else {
+        // display error message if the trainer is not registered
+        console.log("You are not a registered trainer!");
     }
 }
+
+// Function 4: View all available personal trainer sessions
+function viewAvailablePTsessions() {
+    // array to store available sessions
+    const availableSessions = [];
+
+    // check if each trainer has selected sessions 
+    trainerController.trainers.forEach(trainer => {
+        if (trainer.selectedSessions && trainer.selectedSessions.length > 0) {
+            trainer.selectedSessions.forEach(session => {
+                // add the trainer and session to the available sessions array
+                availableSessions.push({ trainer: trainer.name, session: session });
+            });
+        }
+    });
+
+    // format the output for readability
+    const formatOutput = availableSessions.map(entry => {
+        return `${entry.trainer} is available for session ${entry.session}`;
+    });
+    return formatOutput;
+}
+
+// Function 3: Users to view all personal trainers
+function viewAllTrainers() {
+    return trainerController.trainers;
+}
+
+// Function 5: Allow members to book PT sessions based on their fitness goal or desired session time
+function bookGym(name, contact, fitnessGoal, preferredSessionTime) {
+    // create a member object with provided details
+    const member = {
+        name: name,
+        contact: contact,
+        fitnessGoal: fitnessGoal,
+        preferredSessionTime: preferredSessionTime,
+    };
+
+    // check if a preferred session time is provided
+    if (preferredSessionTime) {
+        // check if a fitness goal is provided
+        if (fitnessGoal) {
+            // filter available trainers based on the preferred session time and fitness goal
+            const availableTrainers = trainerController.trainers.filter((trainer) =>
+                trainer.selectedSessions.includes(preferredSessionTime) && trainer.specialty.toLowerCase().includes(fitnessGoal.toLowerCase())
+            );
+
+            // if available trainers are found
+            if (availableTrainers.length > 0) {
+                // find the booked session in the time slots
+                const bookedSession = memberController.timeslots.find(
+                    (session) => session.time === preferredSessionTime
+                );
+
+                // if the session is found
+                if (bookedSession) {
+                    // update session details and assigned trainer
+                    bookedSession.booked = true;
+                    bookedSession.bookedMembers = bookedSession.bookedMembers || [];
+                    bookedSession.bookedMembers.push(member.name);
+
+                    // assign the first available trainer and update selected sessions
+                    const assignedTrainer = availableTrainers[0];
+                    assignedTrainer.selectedSessions = assignedTrainer.selectedSessions.filter(
+                        (session) => session !== preferredSessionTime
+                    );
+
+                    // return a success message
+                    return `Hi ${member.name}, your booking is successful! ${assignedTrainer.name} will be your personal trainer for your fitness goal: ${fitnessGoal.toUpperCase()} at ${preferredSessionTime}.`;
+                } else {
+                    // return a message if the selected session in unavailable
+                    return "Selected session is unavailable. Please choose another time.";
+                }
+            } else {
+                // return a message if no available trainers are found
+                return `Sorry ${name}, no available trainers for your preferred session tie or fitness goal. Please choose another time or specify a different fitness goal.`;
+            }
+        } else {
+            // handle booking for a general session without a fitness goal
+            // find the booked session index in the timeslots
+            const bookedSessionIndex = memberController.timeslots.findIndex(
+                (session) => session.time === preferredSessionTime
+            );
+
+            // if the session is found
+            if (bookedSessionIndex !== -1) {
+                // update session details and assigned trainer
+                const bookedSession = memberController.timeslots[bookedSessionIndex];
+                bookedSession.booked = true;
+                bookedSession.bookedMembers = bookedSession.bookedMembers || [];
+                bookedSession.bookedMembers.push(member.name);
+
+                // find a trainer with the provided session time for a general session
+                const assignedTrainer = trainerController.trainers.find(
+                    (trainer) => trainer.selectedSessions.includes(preferredSessionTime)
+                );
+
+                // if an assigned trainer is found
+                if (assignedTrainer) {
+                    // update the selected sessions of the trainer
+                    assignedTrainer.selectedSessions = assignedTrainer.selectedSessions.filter(
+                        (session) => session !== preferredSessionTime
+                    );
+                }
+
+                // return a success message
+                return `Hi ${member.name}, your booking is successful! You have a general session at ${preferredSessionTime} with no targetted fitness goal with ${assignedTrainer.name}.`;
+            } else {
+                // return a message if the selected session is unavailable
+                return "Selected session is unavailable. Please choose another time.";
+            }
+        }
+    } else {
+        // return a message if no preferred session time is provided
+        return "Please provide your preferred session time.";
+    }
+}
+
+// Function 6: Display members and their registered timing
+function displayAllMemberSchedule() {
+    // array to store members data
+    const membersData = [];
+
+    // check if the session is booked and booked members
+    memberController.timeslots.forEach(session => {
+        if (session.booked && session.bookedMembers && session.bookedMembers.length > 0) {
+            session.bookedMembers.forEach(bookedMember => {
+                membersData.push({
+                    name: bookedMember,
+                    registeredTiming: session.time,
+                });
+            });
+        }
+    });
+    return membersData;
+}
+
+
+
+module.exports = {
+    addTrainer,
+    trainerSelectSessions,
+    viewAvailablePTsessions,
+    viewAllTrainers,
+    bookGym,
+    displayAllMemberSchedule
+};
